dabf476e276a265abedd5dae9f5db5a5ee73f664
Chung Lee <s2econd.blue@gmail.com>
2022-07-25 10:19:12 +0900

    Front 마스터 폴더 정리

--- front/src/App.js
+++ front/src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import logo from "./logo.svg";
-import { Counter } from "./features/counter/Counter";
+import { Counter } from "./ccomponents/Counter";
 import "./App.css";
 
 function App() {
