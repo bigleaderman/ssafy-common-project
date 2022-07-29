451aacb90979747fab8e3cef156cfce326c8f5f7
Lee Chung <tsi03136@naver.com>
2022-07-29 14:02:05 +0900

    merge conflict solved 2

--- front/src/index.js
+++ front/src/index.js
@@ -9,7 +9,6 @@ import { store, persistor } from "./redux/store/store.js";
 import { Provider } from "react-redux";
 import reportWebVitals from "./reportWebVitals";
 
-<<<<<<< HEAD
 //redux-persist 관련 함수
 import { PersistGate } from "redux-persist/integration/react";
 
@@ -21,14 +20,6 @@ ReactDOM.createRoot(root).render(
             <App />
         </PersistGate>
     </Provider>
-=======
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-    //react 컴포넌트에서 store을 사용할 수 있게 Provider 생성
-    <Provider store={store}>
-        <App />
-    </Provider>,
->>>>>>> 12cc2f7d2be64eef9fea24423475bd2009c5af06
 );
 
 reportWebVitals();
--- front/src/pages/CreateNoticePage.js
+++ front/src/pages/CreateNoticePage.js
@@ -1,44 +1,63 @@
-<<<<<<< HEAD
-import React from "react";
-import { Container } from "../style.js";
-=======
 import React, { useState } from "react";
-import { Container, styleTextField, styleButton } from '../style.js';
-import { TextField, Button } from '@mui/material';
-import { useNavigate } from  "react-router-dom";
->>>>>>> 12cc2f7d2be64eef9fea24423475bd2009c5af06
+import { Container, styleTextField, styleButton } from "../style.js";
+import { TextField, Button } from "@mui/material";
+import { useNavigate } from "react-router-dom";
 
 const CreateNoticePage = (props) => {
-  const navigate = useNavigate();
+    const navigate = useNavigate();
 
-  const [title, setTitle] = useState('');
-  const [content, setContent] = useState('');
+    const [title, setTitle] = useState("");
+    const [content, setContent] = useState("");
 
-  const resetForm = () => {
-    setTitle('');
-    setContent('');
-  }
+    const resetForm = () => {
+        setTitle("");
+        setContent("");
+    };
 
-  const goNoticeListPage = () => {
-    navigate("/board");
-  };
+    const goNoticeListPage = () => {
+        navigate("/board");
+    };
 
-  return (
-    <Container>
-      <h2>공지사항 작성 페이지</h2>
+    return (
+        <Container>
+            <h2>공지사항 작성 페이지</h2>
 
-      <label htmlFor="title">제목</label>
-      <TextField style={styleTextField} id="title" name="title" placeholder="제목" value={title} onChange={(e) => {setTitle(e.target.value)}}></TextField>
-      <label htmlFor="content">내용</label>
-      <TextField style={styleTextField} id="content" name="content" placeholder="내용" value={content} onChange={(e) => {setContent(e.target.value)}}></TextField>
-      
-      <span>
-        <Button style={styleButton} onClick={resetForm}>초기화</Button>
-        <Button style={styleButton} onClick={goNoticeListPage}>작성 완료</Button>
-        <Button style={styleButton} onClick={goNoticeListPage}>목록으로</Button>
-      </span>
-    </Container>
-  );
+            <label htmlFor="title">제목</label>
+            <TextField
+                style={styleTextField}
+                id="title"
+                name="title"
+                placeholder="제목"
+                value={title}
+                onChange={(e) => {
+                    setTitle(e.target.value);
+                }}
+            ></TextField>
+            <label htmlFor="content">내용</label>
+            <TextField
+                style={styleTextField}
+                id="content"
+                name="content"
+                placeholder="내용"
+                value={content}
+                onChange={(e) => {
+                    setContent(e.target.value);
+                }}
+            ></TextField>
+
+            <span>
+                <Button style={styleButton} onClick={resetForm}>
+                    초기화
+                </Button>
+                <Button style={styleButton} onClick={goNoticeListPage}>
+                    작성 완료
+                </Button>
+                <Button style={styleButton} onClick={goNoticeListPage}>
+                    목록으로
+                </Button>
+            </span>
+        </Container>
+    );
 };
 
 export default CreateNoticePage;
