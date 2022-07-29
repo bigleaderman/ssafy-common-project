a9a9ce97b4d839d2ea9f364354d2d83315e48b31
kunhee94 <97655672+kunhee94@users.noreply.github.com>
2022-07-29 15:58:33 +0900

    디버깅끝

--- Backend/mafia/src/main/java/com/ssafy/mafia/Controller/AdminController.java
+++ Backend/mafia/src/main/java/com/ssafy/mafia/Controller/AdminController.java
@@ -19,15 +19,22 @@ public class AdminController {
     //유저 전체 리스트 조회
     @GetMapping ("/all/list")
     public ResponseEntity<?> getAllUser() {
-        return new ResponseEntity<List<User>>(adminService.getAllUser(), HttpStatus.OK);
-
+        try {
+            return new ResponseEntity<List<User>>(adminService.getAllUser(), HttpStatus.OK);
+        }catch (Exception e){
+            return new ResponseEntity<String>("잘못된 요청입니다.",HttpStatus.BAD_REQUEST);
+        }
     }
 
     // 레드유저 관리
     @PostMapping("/red/{userSeq}")
     public ResponseEntity<?> redUserControl(@PathVariable("userSeq") int userSeq){
-        adminService.redControl(userSeq);
-        return new ResponseEntity<Void>(HttpStatus.OK);
+        try {
+            adminService.redControl(userSeq);
+            return new ResponseEntity<Void>(HttpStatus.OK);
+        }catch (Exception e){
+            return new ResponseEntity<String>("잘못된 요청입니다.",HttpStatus.BAD_REQUEST);
+        }
     }
 
 
--- Backend/mafia/src/main/java/com/ssafy/mafia/Controller/NoticeController.java
+++ Backend/mafia/src/main/java/com/ssafy/mafia/Controller/NoticeController.java
@@ -12,7 +12,6 @@ import java.util.List;
 
 
 @RestController
-@RequestMapping("/board")
 @RequiredArgsConstructor
 public class NoticeController {
 
@@ -21,46 +20,66 @@ public class NoticeController {
     private final NoticeService noticeService;
 
     //전체 글조회
-    @GetMapping
-    public ResponseEntity<?> getAllNotice() throws Exception {
-        return new ResponseEntity<List<NoticeBoard>>(noticeService.getAllNotice(), HttpStatus.OK);
+    @GetMapping("/board")
+    public ResponseEntity<?> getAllNotice() {
+        try {
+            return new ResponseEntity<List<NoticeBoard>>(noticeService.getAllNotice(), HttpStatus.OK);
+        }catch (Exception e){
+            return new ResponseEntity<String>("잘못된 요청입니다.",HttpStatus.BAD_REQUEST);
+        }
     }
 
     //글번호로 세부조회
-    @GetMapping("/{noticeSeq}")
+    @GetMapping("/board/{noticeSeq}")
     public ResponseEntity<?> getNotice(@PathVariable("noticeSeq") int noticeSeq){
+        try {
+            NoticeBoard noticeBoard = noticeService.noticeDetail(noticeSeq);
 
-        NoticeBoard noticeBoard = noticeService.noticeDetail(noticeSeq);
-
-        return new ResponseEntity<NoticeBoard>(noticeBoard ,HttpStatus.OK);
+            return new ResponseEntity<NoticeBoard>(noticeBoard ,HttpStatus.OK);
+        }catch (Exception e){
+            return new ResponseEntity<String>("잘못된 요청입니다.",HttpStatus.BAD_REQUEST);
+        }
     }
 
     //글삭제
-    @DeleteMapping("/{noticeSeq}")
+    @DeleteMapping("admin/board/{noticeSeq}")
     public ResponseEntity<?> deleteNotice(@PathVariable("noticeSeq") int noticeSeq) {
-        noticeService.deleteNotice(noticeSeq);
-        return new ResponseEntity<Void>(HttpStatus.OK);
+        try {
+            noticeService.deleteNotice(noticeSeq);
+            return new ResponseEntity<Void>(HttpStatus.OK);
+        }catch (Exception e){
+            return new ResponseEntity<String>("잘못된 요청입니다.",HttpStatus.BAD_REQUEST);
+        }
     }
 
 
     //글 작성
-    @PostMapping
+    @PostMapping("/admin/board")
     public ResponseEntity<?> saveNotice(@RequestBody NoticeDto request){
-        NoticeBoard noticeBoard = noticeService.writeNotice(request);
-        return new ResponseEntity<NoticeBoard>(noticeBoard, HttpStatus.OK);
+        try {
+            NoticeBoard noticeBoard = noticeService.writeNotice(request);
+            return new ResponseEntity<NoticeBoard>(noticeBoard, HttpStatus.OK);
+        }catch (Exception e){
+            return new ResponseEntity<String>("잘못된 요청입니다.",HttpStatus.BAD_REQUEST);
+        }
     }
 
     // 글 수정
-    @PutMapping("/{noticeSeq}")
+    @PutMapping("/admin/board/{noticeSeq}")
     public ResponseEntity<?> update(@PathVariable("noticeSeq") int noticeSeq,
                                     @RequestBody NoticeDto request){
-        NoticeBoard noticeBoard = noticeService.updateNotice(noticeSeq, request);
-        return new ResponseEntity<NoticeBoard>(noticeBoard, HttpStatus.OK);
+        try {
+            NoticeBoard noticeBoard = noticeService.updateNotice(noticeSeq, request);
+            return new ResponseEntity<NoticeBoard>(noticeBoard, HttpStatus.OK);
+        }catch (Exception e){
+            return new ResponseEntity<String>("잘못된 요청입니다.",HttpStatus.BAD_REQUEST);
+        }
     }
 
     // 글제목으로 검색
-    @GetMapping("/search/{title}")
+    @GetMapping("/board/search/{title}")
     public ResponseEntity<?> getNoticeByTitle(@PathVariable("title") String title) {
+
         return new ResponseEntity<List<NoticeBoard>>(noticeService.noticeByTitle(title), HttpStatus.OK);
 
     }
