c31bfb8f061fb7d15973352145ded5822c30eede
Lee Chung <tsi03136@naver.com>
2022-08-01 05:10:02 +0900

    S07P12D106-110/맵 구현, 입출력 구현 중

--- front/src/pages/GatherRoomPage.js
+++ front/src/pages/GatherRoomPage.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import MainBar from "../components/GatherRoomPageCom/MainBar";
 import NaviBar from "../components/GatherRoomPageCom/NaviBar";
 import PlayMap from "../components/GatherRoomPageCom/PlayMap";
@@ -11,7 +11,9 @@ export function GatherRoom(props) {
     //현재 방을 클릭했을 때 props로 방 관련 데이터를 받아옴
     //방 번호, 방 제목, 현재 인원, 최대 인원, 시민 수, 의사 수, 경찰 수, 마피아 수
     //각 정보를 하위 컴포넌트들에게 뿌려주기
-    // const [RoomData, setRoomData] = useState(props.RoomData);
+
+    //넘겨받은 방 객체 정보를 저장
+    const [RoomData, setRoomData] = useState(props.RoomData);
     return (
         <Container>
             <Grid container spacing={2}>
@@ -20,7 +22,15 @@ export function GatherRoom(props) {
                     <Grid>
                         <Paper elevation={3}>
                             <MainBar
-                            // RoomTitle={}
+                                // test용도로 임시값 입력
+                                RoomNum={0}
+                                RoomTitle={"Test Title A"}
+                                ConCrnt={1}
+                                ConLimit={8}
+                                CntCivil={5}
+                                CntDoc={1}
+                                CntCop={1}
+                                CntMaf={1}
                             />
                         </Paper>
                     </Grid>
