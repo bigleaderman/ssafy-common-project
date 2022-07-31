d720aeb63f2d92376837294e8c94dc73a8f96d5a
Lee Chung <tsi03136@naver.com>
2022-08-01 05:10:02 +0900

    S07P12D106-110/맵 구현, 입출력 구현 중

--- front/src/components/GatherRoomPageCom/MainBar.js
+++ front/src/components/GatherRoomPageCom/MainBar.js
@@ -1,16 +1,24 @@
 import React from "react";
 import Grid from "@mui/material/Grid";
-export function MainBar() {
+export function MainBar(props) {
     return (
         <Grid container spacing={2}>
             <Grid item xs={4}>
-                빙 번호 : ??? 방 제목 : ???
+                빙 번호 : {props.RoomNum} 방 제목 : {props.RoomTitle}
             </Grid>
             <Grid item xs={4}>
-                <Grid>현재 인원 / 최대 인원 : [현재 인원] / [최대 인원]</Grid>
                 <Grid>
-                    시민 : [???]명 의사 : [???]명 경찰 : [???]명 마피아 :
-                    [???]명
+                    현재 인원 / 최대 인원 : [{props.ConCrnt}] / [
+                    {props.ConLimit}]
+                </Grid>
+                <Grid>
+                    시민 : [{props.CntCivil}]명
+                    <br />
+                    의사 : [{props.CntDoc}]명
+                    <br />
+                    경찰 : [{props.CntCop}]명
+                    <br />
+                    마피아 : [{props.CntMaf}]명
                 </Grid>
             </Grid>
         </Grid>
--- front/src/components/GatherRoomPageCom/NaviBar.js
+++ front/src/components/GatherRoomPageCom/NaviBar.js
@@ -2,12 +2,15 @@ import * as React from "react";
 import Button from "@mui/material/Button";
 import Stack from "@mui/material/Stack";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
-export default function NaviBar() {
+export default function NaviBar(props) {
     const theme = createTheme({
         typography: {
             fontSize: 10,
         },
     });
+    function HandleStartState() {
+        props.onClick(!props.crntStart);
+    }
     return (
         <ThemeProvider theme={theme}>
             <Stack spacing={1} direction="row">
@@ -18,7 +21,9 @@ export default function NaviBar() {
                     환경 설정
                 </Button>
                 <Button variant="contained">도움말</Button>
-                <Button variant="contained">게임 시작</Button>
+                <Button variant="contained" onClick={HandleStartState}>
+                    게임 시작
+                </Button>
                 <Button variant="contained">나가기</Button>
             </Stack>
         </ThemeProvider>
