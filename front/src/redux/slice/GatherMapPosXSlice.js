97d4161d4a20728afa4b8d8dda5697632dfa2018
Chung Lee <s2econd.blue@gmail.com>
2022-08-02 00:42:20 +0900

    S07P12D106-110/아바타조작 구현 중

--- front/src/redux/slice/GatherMapPosXSlice.js
+++ front/src/redux/slice/GatherMapPosXSlice.js
@@ -3,7 +3,11 @@ import { createSlice } from "@reduxjs/toolkit";
 export const GatherMapPosXSlice = createSlice({
     name: "GatherMapPosX",
     initialState: {
+<<<<<<< HEAD
         value: 350,
+=======
+        value: 0,
+>>>>>>> 32528ff (S07P12D106-110/아바타조작 구현 중)
     },
     reducers: {
         SavePosX: (state, action) => {
--- front/src/redux/slice/GatherMapPosYSlice.js
+++ front/src/redux/slice/GatherMapPosYSlice.js
@@ -3,7 +3,11 @@ import { createSlice } from "@reduxjs/toolkit";
 export const GatherMapPosYSlice = createSlice({
     name: "GatherMapPosY",
     initialState: {
+<<<<<<< HEAD
         value: 600,
+=======
+        value: 0,
+>>>>>>> 32528ff (S07P12D106-110/아바타조작 구현 중)
     },
     reducers: {
         SavePosY: (state, action) => {
