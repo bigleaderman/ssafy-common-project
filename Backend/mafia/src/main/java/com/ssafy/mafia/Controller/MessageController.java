3ac262813ba481efbf29793da6542f656ee231ad
siryeongchoi <siryeongchoi@gmail.com>
2022-08-01 22:11:54 +0900

    socket cors error test

--- Backend/mafia/src/main/java/com/ssafy/mafia/Controller/MessageController.java
+++ Backend/mafia/src/main/java/com/ssafy/mafia/Controller/MessageController.java
@@ -4,24 +4,29 @@ import com.ssafy.mafia.Model.InviteDto;
 import com.ssafy.mafia.Model.MessageDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api")
+@CrossOrigin("*")
 public class MessageController {
     private final SimpMessagingTemplate template;
 
     @MessageMapping("/lobby")
-    public void lobbyChat(MessageDto message){
+    public MessageDto lobbyChat(@Payload MessageDto message){
         template.convertAndSend("/sub/lobby", message);
+        return message;
     }
 
     @MessageMapping("/room")
-    public void roomChat(MessageDto message){
+    public MessageDto roomChat(@Payload MessageDto message){
         template.convertAndSend("/sub/room/" + message.getRoomSeq(), message);
+        return message;
     }
 
     @MessageMapping("/invite")
