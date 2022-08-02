587ab67e584e5fdc8ca24ff915d6a2371aafcb17
siryeongchoi <siryeongchoi@gmail.com>
2022-08-02 17:13:40 +0900

    ssl 설정

--- Backend/mafia/src/main/java/com/ssafy/mafia/Config/CorsConfig.java
+++ Backend/mafia/src/main/java/com/ssafy/mafia/Config/CorsConfig.java
@@ -12,6 +12,7 @@ public class CorsConfig implements WebMvcConfigurer {
                 .allowedOrigins("*")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("headers")
+                .allowCredentials(false)
                 .maxAge(3000);
     }
 }
