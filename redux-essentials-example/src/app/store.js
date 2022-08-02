c14d9ed5230e8b7f8023b85a3438110aaf86abd2
Lee Chung <tsi03136@naver.com>
2022-08-02 13:35:02 +0900

    32528ff S07P12D106-110/아바타 충돌 구현

--- redux-essentials-example/src/app/store.js
+++ redux-essentials-example/src/app/store.js
@@ -1,8 +1,26 @@
-import { configureStore } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
+import { configureStore, combineReducers } from "@reduxjs/toolkit";
+
+import counterReducer from "../features/counter/counterSlice";
+
+import { persistReducer, persistStore } from "redux-persist";
+import thunk from "redux-thunk";
+import storage from "redux-persist/lib/storage";
+import storageSession from "redux-persist/lib/storage/session";
+
+const persistConfig = {
+    key: "root",
+    storage,
+    debug: true,
+};
+
+const rootReducer = combineReducers({
+    counter: counterReducer,
+});
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 export const store = configureStore({
-  reducer: {
-    counter: counterReducer,
-  },
+    reducer: persistedReducer,
 });
+
+export const persistor = persistStore(store);
