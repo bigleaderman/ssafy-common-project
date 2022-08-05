6d1c9adb4c833d1605713708fecb441baad6802a
siryeongchoi <siryeongchoi@gmail.com>
2022-08-05 09:35:14 +0900

    스웨어 url 추가

--- Backend/mafia/src/main/java/com/ssafy/mafia/Config/SwaggerConfig.java
+++ Backend/mafia/src/main/java/com/ssafy/mafia/Config/SwaggerConfig.java
@@ -7,12 +7,14 @@ import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiKey;
 import springfox.documentation.service.AuthorizationScope;
 import springfox.documentation.service.SecurityReference;
+import springfox.documentation.service.Server;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 @Configuration
@@ -22,6 +24,7 @@ public class SwaggerConfig {
     @Bean
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
+                .servers(new Server("local", "http://localhost:8000", "localhost", Collections.emptyList(), Collections.emptyList()))
                 .securityContexts(Arrays.asList(securityContext()))
                 .securitySchemes(Arrays.asList(apiKey()))
                 .select()
